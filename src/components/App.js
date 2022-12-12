871216242043b8f5a6359989429b18bca84cbbdd
Winston Langat <winston.langat@student.moringaschool.com>
2022-12-13 01:39:09 +0300

    Modifying routes

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,11 @@ import Login from "./Login"
 import Appointment from "./Appointment"
 import Enroll from "./Enroll"
 import Welcome from "./Welcome"
+import Form from "./Form"
+import Student from './Student';
 import Dashboard from "./Dashboard"
+import LoginS from './studentlogin';
+import UpdateAppointment from './UpdateAppointment';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
 
 
@@ -27,6 +31,15 @@ function App() {
           </Route>
           <Route exact path='/login'>
             <Login />
+          </Route>
+          <Route exact path='/Student'>
+            <Student />
+          </Route>
+          <Route exact path='/studentlogin'>
+          <LoginS  />
+        </Route>
+          <Route exact path='/form'>
+            <Form />
           </Route>
           <Route exact path='/Enroll'>
             <Enroll />
